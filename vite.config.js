7dd73cc3f22cb139ef5a039595aab87c80ac1e31
Haile Dagne <haile.dange0@gmail.com>
2025-09-19 01:05:06 -0400

    changing to lower case

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   plugins: [react()],
-  base: "/Amazon-Clone/",
+  base: "/Amazon-clone/",
   build: {
   outDir: "dist",
   },
